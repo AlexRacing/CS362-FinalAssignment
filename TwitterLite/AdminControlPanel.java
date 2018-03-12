f1e64b913e124fdebe32efceb17c01c4ec4e0ecb
AlexRacing <31743789+AlexRacing@users.noreply.github.com>
2018-03-12 10:53:38 -0300

    Add files via upload

---
+++ TwitterLite/AdminControlPanel.java
@@ -0,0 +1,84 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class AdminControlPanel {
+    JFrame adminCtrFrame;
+    private JPanel primary, left, right, rightUpper, rightLower;
+    private JTextArea treeView;
+    private JTextField userID, groupID;
+    private JScrollPane treeView_scrollPane;
+    private JButton addUser, addGroup, showUserTotal, showGroupTotal, showMessageTotal, showPositivePercent, openUserView;
+    
+    public AdminControlPanel() {
+        adminCtrFrame = new JFrame("Admin Control Panel");
+        adminCtrFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        // ======================= JPanel Instantiation =======================
+        primary = new JPanel();
+        left = new JPanel(); // contains ONLY the TreeView
+        right = new JPanel(); // contains two panels
+        rightUpper = new JPanel();
+        rightLower = new JPanel();
+        // ====================================================================
+        
+        // =================== Add Subpanels and RigidArea ====================
+        primary.add(left, BorderLayout.WEST);
+        primary.add(right, BorderLayout.EAST);
+        
+        right.add(rightUpper);
+        right.add(Box.createRigidArea(new Dimension(0, 178)));
+        right.add(rightLower);
+        // ====================================================================
+        
+        // ======================= Set Layout Managers ========================
+        left.setLayout(new BorderLayout());
+        right.setLayout(new BoxLayout(right, BoxLayout.Y_AXIS));
+        rightUpper.setLayout(new GridLayout(3, 2, 5 ,5));
+        rightLower.setLayout(new GridLayout(2, 2, 5, 5));
+        // ====================================================================
+        
+        // ========================== Create Borders ==========================
+        primary.setBorder(BorderFactory.createEtchedBorder());
+        left.setBorder(BorderFactory.createEtchedBorder());
+        right.setBorder(BorderFactory.createEtchedBorder());
+        // ====================================================================
+        
+        // ====================== Left Panel Components =======================
+        treeView = new JTextArea(20,20);
+        treeView_scrollPane = new JScrollPane(treeView);
+        
+        left.add(treeView_scrollPane);
+        // ====================================================================
+        
+        // =================== Right Upper Panel Components ===================
+        userID = new JTextField();
+        addUser = new JButton("Add User");
+        groupID = new JTextField();
+        addGroup = new JButton("Add Group");
+        openUserView = new JButton("Open User View");
+        
+        rightUpper.add(userID);
+        rightUpper.add(addUser);
+        rightUpper.add(groupID);
+        rightUpper.add(addGroup);
+        rightUpper.add(openUserView);
+        //=====================================================================
+        
+        // =================== Right Lower Panel Components ===================
+        showUserTotal = new JButton("Show User Total");
+        showGroupTotal = new JButton("Show Group Total");
+        showMessageTotal = new JButton("Show Message Total");
+        showPositivePercent = new JButton("Show Positive Percentage");
+        
+        rightLower.add(showUserTotal);
+        rightLower.add(showGroupTotal);
+        rightLower.add(showMessageTotal);
+        rightLower.add(showPositivePercent);
+        // ====================================================================
+        
+        adminCtrFrame.setResizable(false); // prevents the frame from being resized by the user
+        adminCtrFrame.getContentPane().add(primary);
+        adminCtrFrame.pack();
+        adminCtrFrame.setVisible(true);
+    }
+}
---
+++ TwitterLite/UserView.java
@@ -0,0 +1,81 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class UserView {
+    JFrame userViewFrame;
+    JPanel primary, top, middleTop, middleBottom, bottom;
+    JTextField userID;
+    JScrollPane tweetMessage_scroll, currentFollowing_scroll, newsFeed_scroll;
+    JTextArea tweetMessage, currentFollowing, newsFeed;
+    JButton followUser, postTweet;
+    
+    public UserView() {
+        userViewFrame = new JFrame("User View");
+        userViewFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        // ======================= JPanel Instantiation =======================
+        primary = new JPanel();
+        top = new JPanel();
+        middleTop = new JPanel();
+        middleBottom = new JPanel();
+        bottom = new JPanel();
+        // ====================================================================
+        
+        // =================== Add Subpanels and RigidArea ====================
+        primary.add(top);
+        primary.add(middleTop);
+        primary.add(middleBottom);
+        primary.add(bottom);
+        // ====================================================================
+        
+        // ======================= Set Layout Managers ========================
+        primary.setLayout(new BoxLayout(primary, BoxLayout.Y_AXIS));
+        top.setLayout(new GridLayout(1, 2, 5 ,5));
+        middleBottom.setLayout(new GridLayout(1, 2, 5 ,5));
+        // ====================================================================
+        
+        // ========================== Create Borders ==========================
+        primary.setBorder(BorderFactory.createEtchedBorder());
+        top.setBorder(BorderFactory.createEtchedBorder());
+        middleTop.setBorder(BorderFactory.createEtchedBorder());
+        middleBottom.setBorder(BorderFactory.createEtchedBorder());
+        bottom.setBorder(BorderFactory.createEtchedBorder());
+        // ====================================================================
+        
+        // ========================== top Components ==========================
+        userID = new JTextField();
+        followUser = new JButton("Follow User");
+        
+        top.add(userID);
+        top.add(followUser);
+        // ====================================================================
+        
+        // ======================= middleTop Components =======================
+        currentFollowing = new JTextArea(10, 20);
+        currentFollowing_scroll = new JScrollPane(currentFollowing);
+        
+        middleTop.add(currentFollowing_scroll);
+        // ====================================================================
+        
+        // ===================== middleBottom Components ======================
+        tweetMessage = new JTextArea(5,10);
+        postTweet = new JButton("Post Tweet");
+        tweetMessage_scroll = new JScrollPane(tweetMessage);
+        
+        middleBottom.add(tweetMessage_scroll);
+        middleBottom.add(postTweet);
+        // ====================================================================
+        
+        // ======================== bottom Components =========================
+        newsFeed = new JTextArea(10, 20);
+        newsFeed_scroll = new JScrollPane(newsFeed);
+        
+        bottom.add(newsFeed);
+        // ====================================================================
+        
+        userViewFrame.setResizable(false); // prevents the frame from being resized by the user
+        userViewFrame.getContentPane().add(primary);
+        userViewFrame.pack();
+        userViewFrame.setVisible(true);
+    }
+}
