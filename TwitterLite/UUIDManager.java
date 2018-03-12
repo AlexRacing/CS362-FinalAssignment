5dedb2c20360eec0944787554491f00925530b18
JarateKing <scmacintosh@upei.ca>
2018-03-12 15:06:23 -0300

    added getNewUUID to uuid manager

--- TwitterLite/UUIDManager.java
+++ TwitterLite/UUIDManager.java
@@ -1,13 +1,20 @@
 public class UUIDManager
 {
     private static UUIDManager instance = new UUIDManager();
+    private static int currentId;
     
     private UUIDManager()
     {
+        currentId = 0;
     }
 
     public UUIDManager getInstance()
     {
         return instance;
+    }
+    
+    public int getNewUUID()
+    {
+        return currentId++;
     }
 }
