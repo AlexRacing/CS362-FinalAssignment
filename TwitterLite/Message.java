9a1ae193478d8048126228b901b92dc188179c67
JarateKing <scmacintosh@upei.ca>
2018-03-14 17:23:08 -0300

    made message functional

--- TwitterLite/Message.java
+++ TwitterLite/Message.java
@@ -1,6 +1,28 @@
 public class Message
 {
-    public Message()
+    int uuid;
+    IUser op;
+    String message;
+    
+    public Message(IUser op, String message)
     {
+        uuid = UUIDManager.getInstance().getNewUUID();
+        this.op = op;
+        this.message = message;
+    }
+    
+    public getUUID()
+    {
+        return uuid;
+    }
+    
+    public getOP()
+    {
+        return op;
+    }
+    
+    public getMessage()
+    {
+        return message;
     }
 }
