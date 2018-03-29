03eefc66b59ce3c925c1d7c376f958a8d974f5aa
Alex <amdavison@upei.ca>
2018-03-29 17:42:29 -0300

    JTree is updating (more or less) correctly. Need to reload the tree probably in the selectionListener. Currently, Users can be added along with UserGroups in which more users can be added to.

--- TwitterLite/AdminControlPanel.java
+++ TwitterLite/AdminControlPanel.java
@@ -4,6 +4,8 @@ import java.awt.*;
 import javax.swing.*;
 import javax.swing.event.TreeSelectionEvent;
 import javax.swing.event.TreeSelectionListener;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -15,18 +17,21 @@ public class AdminControlPanel {
     private JScrollPane treeView_scrollPane;
     private JTree tree;
     private TreeNodeAdapter root;
+    private DefaultTreeModel model;
     private UserGroup ugRoot = new UserGroup("Root");
+    private UserGroup currentGroup;
     private JButton addUser, addGroup, showUserTotal, showGroupTotal, showMessageTotal, showPositivePercent, openUserView;
-    
+
     public AdminControlPanel() {
 
         root = new TreeNodeAdapter(ugRoot);
         tree = new JTree(root);
         tree.addTreeSelectionListener(new SelectionListener());
+        model = (DefaultTreeModel)tree.getModel();
 
         adminCtrFrame = new JFrame("Admin Control Panel");
         adminCtrFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
+
         // ======================= JPanel Instantiation =======================
         primary = new JPanel();
         left = new JPanel(); // contains ONLY the TreeView
@@ -35,29 +40,29 @@ public class AdminControlPanel {
         rightUpper = new JPanel();
         rightLower = new JPanel();
         // ====================================================================
-        
+
         // =================== Add Subpanels and RigidArea ====================
         primary.add(left, BorderLayout.WEST);
         primary.add(right, BorderLayout.EAST);
-        
+
         right.add(rightUpper);
         right.add(Box.createRigidArea(new Dimension(0, 178)));
         right.add(rightLower);
         // ====================================================================
-        
+
         // ======================= Set Layout Managers ========================
         left.setLayout(new BorderLayout());
         right.setLayout(new BoxLayout(right, BoxLayout.Y_AXIS));
         rightUpper.setLayout(new GridLayout(3, 2, 5 ,5));
         rightLower.setLayout(new GridLayout(2, 2, 5, 5));
         // ====================================================================
-        
+
         // ========================== Create Borders ==========================
         primary.setBorder(BorderFactory.createEtchedBorder());
         left.setBorder(BorderFactory.createEtchedBorder());
         right.setBorder(BorderFactory.createEtchedBorder());
         // ====================================================================
-        
+
         // ====================== Left Panel Components =======================
         treeView_scrollPane = new JScrollPane(tree);
         treeView_scrollPane.setHorizontalScrollBar(treeView_scrollPane.createHorizontalScrollBar());
@@ -65,7 +70,7 @@ public class AdminControlPanel {
 
         left.add(treeView_scrollPane);
         // ====================================================================
-        
+
         // =================== Right Upper Panel Components ===================
         userID = new JTextField();
         addUser = new JButton("Add User");
@@ -83,7 +88,7 @@ public class AdminControlPanel {
         rightUpper.add(addGroup);
         rightUpper.add(openUserView);
         //=====================================================================
-        
+
         // =================== Right Lower Panel Components ===================
         showUserTotal = new JButton("Show User Total");
         showGroupTotal = new JButton("Show Group Total");
@@ -100,7 +105,7 @@ public class AdminControlPanel {
         rightLower.add(showMessageTotal);
         rightLower.add(showPositivePercent);
         // ====================================================================
-        
+
         adminCtrFrame.setResizable(false); // prevents the frame from being resized by the users
         adminCtrFrame.getContentPane().add(primary);
         adminCtrFrame.pack();
@@ -109,17 +114,18 @@ public class AdminControlPanel {
 
     public class addUserAL implements ActionListener {
         public void actionPerformed(ActionEvent event) {
-            //root.add(new TreeNodeAdapter(ugRoot.spawnUser(userID.getText())));
-            ugRoot.spawnUser(userID.getText());
-            //ugRoot.acceptVisitor(new TestVisitor());
+            ugRoot.spawnUserGroup(userID.getText());
+            ugRoot.acceptVisitor(new TestVisitor());
+
+            try { model.reload(root); } catch(NullPointerException npE) { System.out.println(""); }
         }
     }
 
     public class addGroupAL implements ActionListener {
         public void actionPerformed(ActionEvent event) {
-            //root.add(new TreeNodeAdapter(ugRoot.spawnUserGroup(groupID.getText())));
             ugRoot.spawnUserGroup(groupID.getText());
-            //ugRoot.acceptVisitor(new TestVisitor());
+
+            try { model.reload(root); } catch(NullPointerException npE) { System.out.println(""); }
         }
     }
 
@@ -150,11 +156,22 @@ public class AdminControlPanel {
 
     public class SelectionListener implements TreeSelectionListener {
         public void valueChanged(TreeSelectionEvent se) {
-            JTree tree = (JTree) se.getSource();
-            TreeNodeAdapter selectedNode = (TreeNodeAdapter) tree.getLastSelectedPathComponent();
-            String selectedNodeName = selectedNode.toString();
-            if(selectedNode.isLeaf())
-                System.out.println(selectedNodeName); // does nothing useful but shows listener is working
+            tree = (JTree) se.getSource();
+
+            ugRoot = (UserGroup) ((TreeNodeAdapter) tree.getLastSelectedPathComponent()).getUserObject();
+
+            // ========= CLEAN UP LATER =========
+            //System.out.println(tree.getLastSelectedPathComponent());
+            //System.out.println(tree.getLastSelectedPathComponent().toString());
+            //System.out.println(tree.getLastSelectedPathComponent().getClass());
+            //ugRoot = (UserGroup) tree.getLastSelectedPathComponent();
+
+            //root = (TreeNodeAdapter)tree.getLastSelectedPathComponent();
+            //TreeNodeAdapter selectedNode = (TreeNodeAdapter) tree.getLastSelectedPathComponent();
+
+            //AbstractUser u = (AbstractUser) ((TreeNodeAdapter) tree.getLastSelectedPathComponent()).getUserObject(); // *******
+            //System.out.println(u);
+            // ==================================
         }
     }
 }
